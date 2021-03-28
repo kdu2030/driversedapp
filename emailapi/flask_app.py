dac6efa989e5321740f883175ba39a1d120c647b
kdu2030 <77704011+kdu2030@users.noreply.github.com>
2021-03-28 11:10:05 -0400

    Update flask_app.py

--- emailapi/flask_app.py
+++ emailapi/flask_app.py
@@ -12,8 +12,9 @@ mail = Mail(app)
 
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
-app.config['MAIL_USERNAME'] = 'thecoldprogrammer@gmail.com'
-app.config['MAIL_PASSWORD'] = 'SenM@rcoRub1o'
+#MAIL_USERNAME and MAIL_PASSWORD are not real
+app.config['MAIL_USERNAME'] = 'notanemail@email.com'
+app.config['MAIL_PASSWORD'] = 'notapassword'
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USE_SSL'] = False
 mail = Mail(app)
